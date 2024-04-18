26177c656c5098f84c198c3ecc0c81ee07e9b93b
@Gautam- <>
2024-04-18 15:03:27 +0530

    Initial commit

---
+++ basic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    // printf("Gautam surolia\n");
+
+    int a, b;
+ 
+    printf("Enter the number a\n");
+    scanf("%d", &a);
+
+    printf("Enter the number b\n");
+    scanf("%d", &b);
+
+    printf("The sum is %d\n", a+b);
+    return 0;                      
+} 
